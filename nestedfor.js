8282ff2655ea12d759478aca48bcafca7f02d888
ngenziwingenzi <nshutihubert4@gmail.com>
2025-06-23 12:08:27 +0300

    Adding a new concept

---
+++ nestedfor.js
@@ -0,0 +1,25 @@
+// Using arrays of arrays to replace even numbers with the word "even" and odd nbrs with the word "odd"
+
+var numbers = [
+    [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+    [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+    [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+    [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+    [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+    [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+    [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+    [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+    [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+    [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
+];
+
+for (rows = 0; rows < numbers.length; rows++){
+    for(cols = 0; cols < numbers[rows].length; cols++){
+        if(numbers[rows][cols]%2 === 0)
+        numbers[rows][cols] = "even";
+        else {
+            numbers[rows][cols] = "odd";
+        }
+    }
+}
+console.log(numbers);
